07bf404665a41f646bc8718250f61ca3deb1e2ff
Matas <matasalzo@gmail.com>
2022-01-02 13:00:18 +0200

    added changes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <isHeader />
+  <isBody />
 </template>
 
 <script>
 import isHeader from './components/isHeader.vue'
-
+import isBody from './components/IsBody.vue'
+import IsBody from './components/IsBody.vue'
 export default {
   name: 'App',
   components: {
     isHeader,
+    IsBody,
   }
 }
 </script>
--- src/components/isHeader.vue
+++ src/components/isHeader.vue
@@ -1,25 +1,51 @@
+<head>
+<link rel="stylesheet" href="path/to/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
 <template>
   <div id="header">
       <div class="navbar">
-        <a class="logo" href="">
-            <img src="../assets/logo.png" alt="">
-        </a>
-        <a class="list" href="">home</a>
-        <a class="list" href="">projects</a>
-        <a class="list" href="">about me</a>
-        <a class="list" href="">contact</a>
+          <button class="hamburger" @click="navOn = !navOn">
+            <span class="burger">☰</span>
+        </button>
+        <div class="logo">
+            <img class="img" src="../assets/logo.png" alt="">
+        </div>
+        <ul class="navigation" :class="{show: navOn}">
+            <a href="#"><li>home</li></a>
+            <a href="#"><li>projects</li></a>
+            <a href="#"><li>about me</li></a>
+            <a href="#"><li>contact</li></a>
+        </ul>
       </div>
+            <ul class="navigationburger" :class="{show: navOn}">
+            <a href="#"><li>home</li></a>
+            <a href="#"><li>projects</li></a>
+            <a href="#"><li>about me</li></a>
+            <a href="#"><li>contact</li></a>
+        </ul>
   </div>
 </template>
 
 <script>
+
 export default {
     name: "isHeader",
-
+    data: function (){
+        return {
+            navOn: false,
+        }
+    }
 }
 </script>
-
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
 body{
     margin-left: 0px;
 }
@@ -29,45 +55,109 @@ body{
      text-decoration: none;
      height: 60px;
      width: 100%;
-
      position: fixed;
      top: 0px;
-     display: flex;
-
-
+     display: block;
 }
- .navbar{
-    color: white;
-    width: 60%;
-    
-    display: felx;
-
-
-    height: 40px;
-    margin: auto;
-    height: auto;
- }
-
-.list {
-    color: F8F8F8;
-    text-decoration: none;
-    display: felx;
-
-    color: white;
-    height: auto;
-    padding: 0px 20px;
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
-    float: right;
-
-
- }
-.logo img {
-    height: 40px;
-    width: 40px;
+.navbar {
+    width: 80%;
+    height: 100%;
+    margin: 0 auto;
+    text-align: center;
+}
+.logo {
     float: left;
-    display: flex;
+    height: 100%;
+    width: 50px;
+    display: grid;
+}
+.img {
+    width: 35px;
+    height: 35px;
+    margin: auto;
+}
 
-    
+.navigation {
+    padding: 0 auto;
+    padding-top: 0px;
+    float: right;
+    height: 100%;
+}
+.navigation a {
+    height: 100%;
+    display: table;
+    float: left;
+    padding-left: 25px;
+    text-decoration: none;
+}
+.navigation a li:hover{
+    color: #6D83F2;
+}
+.navigation a li {
+    display: table-cell;
+    vertical-align: middle;
+    color: #f8f8f8;
+    font-size: 14px;
+    font-family: 'Montserrat', sans-serif;
+}
+.navigationburger {
+    display: none;
+}
+.hamburger {
+display: none;
+}
+@media screen and (max-width: 768px) {
 
 }
+@media screen and (max-width: 500px) {
+    .navigation {
+        display: none;
+    }
+    .navigationburger.show {
+        float: right;
+        display: grid;
+        background-color: #232323;
+        border-bottom-left-radius: 5px;
+        height: 500px;
+        width: 60%;
+        align-items: center;
+        justify-content: center;
+        grid-template-columns: 1fr;
+        grid-template-rows: repeat(8, 1fr);
+    }
+    .navigationburger.show a{
+        text-decoration: none;
+        color: #f8f8f8;
+        list-style: none;
+    }
+    .navigationburger.show a:hover{
+        color: #6D83F2;
+    }
+
+    .hamburger {
+    background-color: transparent;
+    color: white;
+    border: 0;
+    float: right;
+    height: 100%;
+    display: grid;
+    cursor: pointer;
+    width: 50px;
+}
+    .hamburger:hover {
+        color: #6D83F2;
+    }
+    .burger {
+        margin: auto;
+        float: right;
+        font-size: 27px;
+        
+    }
+}
+@media screen and (max-width: 350px) {
+        .navigation a li{
+            font-size: 10px;
+        }
+}
+
 </style>
